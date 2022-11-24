e742005cb0ebcc64344fd33fa1cb2fcb7bf8ad67
Ben Burke <ben@sydneytech.org>
2022-11-24 13:14:22 +1100

    first commit

--- mpv_function_lib.lua
+++ mpv_function_lib.lua
@@ -1,1 +1,17 @@
-local utils = require("mp.utils")^
+-- this module to provide the reusable code
+local utils = require("mp.utils")
+
+
+function validate_environment()
+    -- check for environment variables... and...
+    environment_variables = {"VIDEOLIB", "M3ULIB", "EDLLIB", "SUBTITLESLIB"}
+    l = environment_variables
+    while l do
+        print(l.value)
+        l = l.next
+    end
+end
+
+
+
+validate_environment()
