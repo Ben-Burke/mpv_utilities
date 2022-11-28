1715d8057b9fd4bad4859b8c43b01f2e1fb8a947
Ben Burke <ben@sydneytech.org>
2022-11-29 09:43:39 +1100

    initial

--- mpv_function_lib.lua
+++ mpv_function_lib.lua
@@ -7,11 +7,29 @@ function validate_environment()
     environment_variables = {"VIDEOLIB", "M3ULIB", "EDLLIB", "SUBTITLESLIB"}
     l = environment_variables
     while l do
-        print(l.value)
+        print (i)
+        print(l.value) 
         l = l.next
     end
 end
 
-
-
 validate_environment()
+print("good morning")
+
+function allwords ()
+    local line = io.read()  -- current line
+    local pos = 1           -- current position in the line
+    return function ()      -- iterator function
+      while line do         -- repeat while there are lines
+        local s, e = string.find(line, "%w+", pos)
+        if s then           -- found a word?
+          pos = e + 1       -- next position is after this word
+          return string.sub(line, s, e)     -- return the word
+        else
+          line = io.read()  -- word not found; try next line
+          pos = 1           -- restart from first position
+        end
+      end
+      return nil            -- no more lines: end of traversal
+    end
+  end
